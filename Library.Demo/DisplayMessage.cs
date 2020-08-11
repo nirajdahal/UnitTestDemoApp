69db785e68037d81de28b33b67f1e0bb4964c5eb
nirajdahal09@gmail.com <nirajdahal09@gmail.com>
2020-08-11 13:40:24 +0545

    Add project files.

---
+++ Library.Demo/DisplayMessage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Demo
+{
+    public class DisplayMessage
+    {
+        public string Greeting(string firstName, int hourOfTheDay)
+        {
+            string greetings = "";
+            if (hourOfTheDay < 5)
+            {
+                greetings = $"Go To Bed {firstName}";
+            }
+            else if (hourOfTheDay < 12)
+            {
+                greetings = $"Good Morning {firstName}";
+            }
+
+            else if (hourOfTheDay < 18)
+            {
+                greetings = $"Good Afternoon {firstName}";
+            }
+
+            else
+            {
+                greetings = $"Good Evening {firstName}";
+            }
+            return greetings;
+        }
+
+
+      
+    }
+}
---
+++ Library.Tests/DisplayMessagesTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Library.Demo;
+
+
+//Arrange, Act, Assert
+//expected
+namespace Library.Tests
+{
+    public class DisplayMessagesTests
+    {
+        [Fact]
+        public void GreetingShouldReturnGoodMorningMessage()
+        {
+            //Arrange
+            DisplayMessage messages = new DisplayMessage();
+            string expected = "Go To Bed Niraj";
+
+            //Act
+            string actual = messages.Greeting("Niraj", 2);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GreetingShouldReturnGoodAfternoonMessage()
+        {
+            //Arrange
+            DisplayMessage messages = new DisplayMessage();
+            string expected = "Good Afternoon Niraj";
+
+            //Act
+            string actual = messages.Greeting("Niraj", 14);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("Niraj", 0, "Go To Bed Niraj")]
+        [InlineData("Niraj", 1, "Go To Bed Niraj")]
+        [InlineData("Niraj", 2, "Go To Bed Niraj")]
+        [InlineData("Niraj", 3, "Go To Bed Niraj")]
+        [InlineData("Niraj", 4, "Go To Bed Niraj")]
+        [InlineData("Niraj", 5, "Good Morning Niraj")]
+        [InlineData("Niraj", 6, "Good Morning Niraj")]
+        [InlineData("Niraj", 7, "Good Morning Niraj")]
+        [InlineData("Niraj", 8, "Good Morning Niraj")]
+        [InlineData("Niraj", 9, "Good Morning Niraj")]
+        [InlineData("Niraj", 10, "Good Morning Niraj")]
+        [InlineData("Niraj", 11, "Good Morning Niraj")]
+        [InlineData("Niraj", 12, "Good Afternoon Niraj")]
+        [InlineData("Niraj", 13, "Good Afternoon Niraj")]
+        [InlineData("Niraj", 14, "Good Afternoon Niraj")]
+        public void GreetingsShouldReturnExpectedValue(string firstName, int hourOfTheDay, string expected)
+        {
+            //Arrange
+            DisplayMessage messages = new DisplayMessage();
+            
+
+            //Act
+            string actual = messages.Greeting(firstName, hourOfTheDay);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
